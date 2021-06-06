240a6a5a9c0c6228ff6c36146b07f7df2dad3556
xdesprox <xdesprox>
2021-06-06 00:04:22 -0600

    Mostrar datos y busqueda Modulo Usuarios

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -2,28 +2,42 @@
 package controlador;
 
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import modelos.Usuario;
+import modelos.dao.UsuarioDao;
 import utilidades.CambiaPanel;
+import utilidades.ImgTabla;
 import vistas.main.Login;
 import vistas.main.Menu;
 import vistas.modulos.Dashboard;
-import vistas.modulos.NuevoUsuario;
+import vistas.modulos.ModalUsuario;
 import vistas.modulos.VistaUsuario;
 
-public class Controlador implements MouseListener{
+public class Controlador implements MouseListener, KeyListener{
+    DefaultTableModel modelo;
     private Menu menu;
     private Login login;
     private String principalOn = "";
     private String modalOn = "";
     
+    
     /* CONTROL DE USUARIOS */
     Usuario usuario = new Usuario();
     Usuario usuarioSelected = null;
+    UsuarioDao usuarioDao = new UsuarioDao();
     VistaUsuario vistaUsuario;
-    NuevoUsuario nuevoUsuario;
+    ModalUsuario modalUsuario;
     
 
     public Controlador(Menu menu) {
@@ -46,15 +60,87 @@ public class Controlador implements MouseListener{
             vistaUsuario.setControlador(this);
             principalOn = "Usuarios";
             new CambiaPanel(menu.body, vistaUsuario);
+            mostrarDatos(vistaUsuario.tablaUsuarios);
         }else if(vista.equals("AgregarUsuario")){
-            nuevoUsuario = new NuevoUsuario(new JFrame(), true, vistaUsuario);
-            nuevoUsuario.iniciar();
+            modalUsuario = new ModalUsuario(new JFrame(), true, vistaUsuario);
+            modalUsuario.iniciar();
         }
     }
+    
+    public void mostrarDatos(JTable tabla){
+          
+        DefaultTableCellRenderer diseño = (DefaultTableCellRenderer) tabla.getCellRenderer(0, 0); //Obtener diseño de la tabla
+        modelo = (DefaultTableModel)tabla.getModel();
+        modelo.setRowCount(0);
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
+        /* CONTROL DE USUARIOS */
+        if(principalOn.equals("Usuarios")){
+            tabla.setDefaultRenderer(Object.class, new ImgTabla()); //Renderizar para poner las img
+            
+            tabla.getColumnModel().getColumn(0).setCellRenderer(diseño); //Mantener diseño de la tabla por columns
+            tabla.getColumnModel().getColumn(1).setCellRenderer(diseño);
+            tabla.getColumnModel().getColumn(2).setCellRenderer(diseño);
+            tabla.getColumnModel().getColumn(3).setCellRenderer(diseño);
+            
+            ArrayList<Usuario> usuarios = usuarioDao.selectAll();
+            int i = 1;
+            
+            for(Usuario x : usuarios){
+                
+                ImageIcon img_edit = new ImageIcon(getClass().getResource("/img/editar.png"));
+                JLabel lbImg_edit = new JLabel(new ImageIcon(img_edit.getImage()));
+
+                ImageIcon img_delete = new ImageIcon(getClass().getResource("/img/delete.png"));
+                JLabel lbImg_delete = new JLabel(new ImageIcon(img_delete.getImage()));
+
+                ImageIcon img_change = new ImageIcon(getClass().getResource("/img/key_22px.png"));
+                JLabel lbImg_change = new JLabel(new ImageIcon(img_change.getImage()));
+                
+                modelo.addRow(new Object[]{i, x.getNickname(), x.getClave(), x.getRol(), lbImg_edit, lbImg_delete, lbImg_change});
+                i++;
+            }
+            
+            tabla.setModel(modelo);
+        }
+    }
+    
+    public void mostrarBusqueda(ArrayList lista, JTable tabla){
         
+        DefaultTableCellRenderer diseño = (DefaultTableCellRenderer) tabla.getCellRenderer(0, 0); //Obtener diseño de la tabla
+        modelo = (DefaultTableModel)tabla.getModel();
+        modelo.setRowCount(0);
+        
+        /* CONTROL DE USUARIOS */
+        if(principalOn.equals("Usuarios")){
+            tabla.setDefaultRenderer(Object.class, new ImgTabla()); //Renderizar para poner las img
+            
+            tabla.getColumnModel().getColumn(0).setCellRenderer(diseño); //Mantener diseño de la tabla por columns
+            tabla.getColumnModel().getColumn(1).setCellRenderer(diseño);
+            tabla.getColumnModel().getColumn(2).setCellRenderer(diseño);
+            tabla.getColumnModel().getColumn(3).setCellRenderer(diseño);
+            
+            int i = 1;
+            
+            for(Object obj : lista){
+                
+                Usuario x = (Usuario) obj;
+                
+                ImageIcon img_edit = new ImageIcon(getClass().getResource("/img/editar.png"));
+                JLabel lbImg_edit = new JLabel(new ImageIcon(img_edit.getImage()));
+
+                ImageIcon img_delete = new ImageIcon(getClass().getResource("/img/delete.png"));
+                JLabel lbImg_delete = new JLabel(new ImageIcon(img_delete.getImage()));
+
+                ImageIcon img_change = new ImageIcon(getClass().getResource("/img/key_22px.png"));
+                JLabel lbImg_change = new JLabel(new ImageIcon(img_change.getImage()));
+                
+                modelo.addRow(new Object[]{i, x.getNickname(), x.getClave(), x.getRol(), lbImg_edit, lbImg_delete, lbImg_change});
+                i++;
+            }
+            
+            tabla.setModel(modelo);
+        }
+          
     }
 
     @Override
@@ -67,6 +153,25 @@ public class Controlador implements MouseListener{
         }else if(e.getSource().equals(vistaUsuario.btnNuevo)){
             mostrarVista("AgregarUsuario");
         }
+    }
+    
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+        /* CONTROL DE USUARIOS */
+        if(principalOn.equals("Usuarios")){
+            ArrayList<Usuario> lista = usuarioDao.buscar(vistaUsuario.tfBusqueda.getText() + e.getKeyChar());
+            if(lista.isEmpty()){
+                mostrarDatos(vistaUsuario.tablaUsuarios);
+            }else{
+                mostrarBusqueda(lista, vistaUsuario.tablaUsuarios);
+            }
+        }
+    }
+    
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
     }
 
     @Override
@@ -82,6 +187,16 @@ public class Controlador implements MouseListener{
     @Override
     public void mouseExited(MouseEvent me) {
        
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        
     }
 
     
